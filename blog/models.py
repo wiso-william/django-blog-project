e5f1a92bbe77f5b3f7cea108b4d4889d1b321e21
wiso-william <williammancy0@gmail.com>
2025-04-26 21:27:33 +0200

    added posts, tag, models, superuser etc...

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=30)
+    def __str__(self):
+        return self.caption
+
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100)
+    email_adress = models.EmailField() #automatic validator
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return self.full_name()
+
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
@@ -9,3 +25,8 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True,db_index=True) #automatically indexed, unique assumes it as well
     content = models.TextField(validators=[MinLengthValidator(20)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL,null=True ,related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+
+#helper function
