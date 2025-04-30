0c0629461bd50cc2b97ebcb2ebb8af9bc193ccdd
wiso-william <williammancy0@gmail.com>
2025-04-30 15:22:19 +0200

    Added file upload functionality

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts",null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True,db_index=True) #automatically indexed, unique assumes it as well
     content = models.TextField(validators=[MinLengthValidator(20)])
